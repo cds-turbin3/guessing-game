a7fd31492fe168b0ebdb091038cbd829bf2fb256
cds-amal <cds.sudama@gmail.com>
2026-05-01 18:51:55 -0400

    Treat like binary search

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let max = 100000;
+    let secret_number = rand::thread_rng().gen_range(1..=max);
 
     println!("Guess the number!");
+    let mut left_bound = 0;
+    let mut right_bound = max;
+    let mut mid_point;
 
     loop {
-        print!("Please input your guess: ");
+        eprint!("Please input your guess: ");
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -19,11 +23,19 @@ fn main() {
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        // println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
-            std::cmp::Ordering::Less => println!("Too small!"),
-            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Less => {
+                left_bound = guess;
+                mid_point = (left_bound + right_bound) / 2;
+                println!("Too small! {left_bound:?} <- [{mid_point:?}] -> {right_bound:?}");
+            }
+            std::cmp::Ordering::Greater => {
+                right_bound = guess;
+                mid_point = (left_bound + right_bound) / 2;
+                println!("Too Large! {left_bound:?} <- [{mid_point:?}] -> {right_bound:?}");
+            }
             std::cmp::Ordering::Equal => {
                 println!("You win!");
                 break;
